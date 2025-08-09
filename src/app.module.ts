4dc498e30c0fb24bfaaf2773d46d6bd6be83e7ca
accela673 <sardarkasmaliev@gmail.com>
2025-08-09 23:39:48 +0600

    added modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
+import { RoomsModule } from './rooms/rooms.module';
+import { BookingsModule } from './bookings/bookings.module';
+import { AuthModule } from './auth/auth.module';
+import { NotificationsModule } from './notifications/notifications.module';
 
 @Module({
   imports: [
@@ -9,6 +14,11 @@ import { ConfigModule } from '@nestjs/config';
       isGlobal: true, // доступен во всех модулях без повторного импорта
       envFilePath: '.env', // можно указать массив путей
     }),
+    UsersModule,
+    RoomsModule,
+    BookingsModule,
+    AuthModule,
+    NotificationsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
