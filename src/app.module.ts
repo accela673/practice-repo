c179ac645cc322c066f64ccd0a4bb9462bf48fa1
accela673 <sardar.kasmaliev@alatoo.edu.kg>
2025-08-09 23:30:29 +0600

    added database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // доступен во всех модулях без повторного импорта
+      envFilePath: '.env', // можно указать массив путей
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const PORT = process.env.PORT || 3000;
+  await app.listen(PORT);
+  Logger.log(`🚀 App is running on: http://localhost:${PORT}`);
 }
 bootstrap();
